424ef4acc79309b1b10911a1b932c72dc424dda2
Mikhail <mk0602604@gmail.com>
2023-02-06 22:03:29 +0300

    Add validation for environment variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PaletteModule } from './palette/palette.module';
 import { UserModule } from './user/user.module';
+import { validate } from './validation/env.validation';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, validate }),
     AuthModule,
     UserModule,
     PaletteModule,
