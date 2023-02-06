830b96e376612c9e64c9079c98ecfef22b09dc94
Mikhail <mk0602604@gmail.com>
2023-02-06 17:49:22 +0300

    Add config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { PaletteModule } from './palette/palette.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PaletteModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    PaletteModule,
+  ],
 })
 export class AppModule {}
