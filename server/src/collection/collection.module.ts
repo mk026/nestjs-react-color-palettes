02281e04338b265971409fe59c901758cb8380a6
Mikhail <mk0602604@gmail.com>
2023-02-07 21:22:35 +0300

    Create and register collection entity

--- server/src/collection/collection.module.ts
+++ server/src/collection/collection.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Collection } from './collection.entity';
 import { CollectionController } from './collection.controller';
 import { CollectionService } from './collection.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Collection])],
   controllers: [CollectionController],
   providers: [CollectionService],
 })
