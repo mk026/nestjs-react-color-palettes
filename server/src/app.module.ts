613021358f57e6f407bc3272f1d67c3e0ba1b68c
Mikhail <mk0602604@gmail.com>
2023-02-07 21:09:53 +0300

    Configure and register typeorm root module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 import { CollectionModule } from './collection/collection.module';
 import { PaletteModule } from './palette/palette.module';
 import { UserModule } from './user/user.module';
 import { validate } from './validation/env.validation';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate }),
+    TypeOrmModule.forRootAsync({ useFactory: typeormConfig }),
     AuthModule,
     UserModule,
     PaletteModule,
