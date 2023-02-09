05374c6307f5d3071f9dd5ca12036e8629771973
Mikhail <mk0602604@gmail.com>
2023-02-09 20:12:02 +0300

    Create signToken helper method

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 import { SigninCredentialsDto } from './dto/signin-credentials.dto';
 import { SignupCredentialsDto } from './dto/signup-credentials.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
   signup(signupCredentialsDto: SignupCredentialsDto) {
     return 'Signup';
   }
 
   signin(signinCredentialsDto: SigninCredentialsDto) {
     return 'Signin';
+  }
+
+  signToken(userId: number) {
+    const payload = { userId };
+    return this.jwtService.sign(payload);
   }
 }
