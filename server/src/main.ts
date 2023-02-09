5865eb027bb7b4c5b7d06cb0d873edff39dd5993
Mikhail <mk0602604@gmail.com>
2023-02-09 20:56:39 +0300

    Use config service to get app port

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(8080);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('PORT'));
+  await app.listen(port);
 }
 bootstrap();
