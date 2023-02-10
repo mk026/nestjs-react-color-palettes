222b53e69f0bfbf87a76bdb6304dd7e80efb96a7
Mikhail <mk0602604@gmail.com>
2023-02-10 23:46:07 +0300

    Add markup to signup/signin forms

--- client/src/modules/signin-form/components/signin-form/SigninForm.tsx
+++ client/src/modules/signin-form/components/signin-form/SigninForm.tsx
@@ -1,7 +1,31 @@
 import { FC } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Box } from "@mui/material";
+
+import { signinValidationSchema } from "../../helpers/signinValidation";
+import FormField from "../../../../components/form-field";
+import LoadingButton from "../../../../components/loading-button";
 
 const SigninForm: FC = () => {
-  return <div>SigninForm</div>;
+  const methods = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(signinValidationSchema),
+  });
+
+  const signinHandler = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <Box component="form" onSubmit={methods.handleSubmit(signinHandler)}>
+        <FormField label="Email" name="email" type="email" />
+        <FormField label="Password" name="password" type="password" />
+        <LoadingButton isLoading={false}>Signin</LoadingButton>
+      </Box>
+    </FormProvider>
+  );
 };
 
 export default SigninForm;
--- client/src/modules/signup-form/components/signup-form/SignupForm.tsx
+++ client/src/modules/signup-form/components/signup-form/SignupForm.tsx
@@ -1,7 +1,38 @@
 import { FC } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Box } from "@mui/material";
+
+import { signupValidationSchema } from "../../helpers/signupValidation";
+import FormField from "../../../../components/form-field";
+import LoadingButton from "../../../../components/loading-button";
 
 const SignupForm: FC = () => {
-  return <div>SignupForm</div>;
+  const methods = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(signupValidationSchema),
+  });
+
+  const signupHandler = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <Box component="form" onSubmit={methods.handleSubmit(signupHandler)}>
+        <FormField label="Name" name="name" />
+        <FormField label="About yourself" name="bio" multiline />
+        <FormField label="Email" name="email" type="email" />
+        <FormField label="Password" name="password" type="password" />
+        <FormField
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+        />
+        <LoadingButton isLoading={false}>Signup</LoadingButton>
+      </Box>
+    </FormProvider>
+  );
 };
 
 export default SignupForm;
