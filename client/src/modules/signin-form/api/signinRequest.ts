be36a58a0263f1e5ba1004657b36e3022b106af3
Mikhail <mk0602604@gmail.com>
2023-02-10 23:26:44 +0300

    Move axios instance into utils

--- client/src/modules/signin-form/api/signinRequest.ts
+++ client/src/modules/signin-form/api/signinRequest.ts
@@ -1,4 +1,4 @@
-import { api } from "../../../api/axiosInstance";
+import { api } from "../../../utils/axiosInstance";
 import { SIGNIN_ENDPOINT_URL } from "../constants";
 
 export const signinRequest = (data: any) => api.post(SIGNIN_ENDPOINT_URL, data);
--- client/src/modules/signup-form/api/signupRequest.ts
+++ client/src/modules/signup-form/api/signupRequest.ts
@@ -1,4 +1,4 @@
-import { api } from "../../../api/axiosInstance";
+import { api } from "../../../utils/axiosInstance";
 import { SIGNUP_ENDPOINT_URL } from "../constants";
 
 export const signupRequest = (data: any) => api.post(SIGNUP_ENDPOINT_URL, data);
