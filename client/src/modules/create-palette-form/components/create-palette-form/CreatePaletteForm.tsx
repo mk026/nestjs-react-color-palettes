365d27bf1658aba5749a1c346b0c8abdc488c9df
Mikhail <mk0602604@gmail.com>
2023-02-13 23:43:41 +0300

    Add markup for create-palette-form

--- client/src/modules/create-palette-form/components/create-palette-form/CreatePaletteForm.tsx
+++ client/src/modules/create-palette-form/components/create-palette-form/CreatePaletteForm.tsx
@@ -1,7 +1,29 @@
 import { FC } from "react";
+import { FormProvider } from "react-hook-form";
+import { Box } from "@mui/material";
+
+import { useCreatePaletteForm } from "../../hooks/useCreatePaletteForm";
+import CreatePaletteButton from "../create-palette-button";
+import FormField from "../../../../components/form-field";
 
 const CreatePaletteForm: FC = () => {
-  return <div>CreatePaletteForm</div>;
+  const methods = useCreatePaletteForm();
+
+  const createPaletteHandler = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <Box
+        component="form"
+        onSubmit={methods.handleSubmit(createPaletteHandler)}
+      >
+        <FormField label="Title" name="title" />
+        <CreatePaletteButton isLoading={false} />
+      </Box>
+    </FormProvider>
+  );
 };
 
 export default CreatePaletteForm;
