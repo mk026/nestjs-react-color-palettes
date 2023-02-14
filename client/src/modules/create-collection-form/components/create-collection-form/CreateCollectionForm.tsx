a757e23c8634753e35a9ecffd15b9c6c3cc2035f
Mikhail <mk0602604@gmail.com>
2023-02-14 23:17:22 +0300

    Add markup for create-collection-form component

--- client/src/modules/create-collection-form/components/create-collection-form/CreateCollectionForm.tsx
+++ client/src/modules/create-collection-form/components/create-collection-form/CreateCollectionForm.tsx
@@ -1,7 +1,29 @@
 import { FC } from "react";
+import { FormProvider } from "react-hook-form";
+import { Box } from "@mui/material";
+
+import { useCreateCollectionForm } from "../../hooks/useCreateCollectionForm";
+import CreateCollectionButton from "../create-collection-button";
+import FormField from "../../../../components/form-field";
 
 const CreateCollectionForm: FC = () => {
-  return <div>CreateCollectionForm</div>;
+  const methods = useCreateCollectionForm();
+
+  const createCollectionHandler = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <Box
+        component="form"
+        onSubmit={methods.handleSubmit(createCollectionHandler)}
+      >
+        <FormField label="Title" name="title" />
+        <CreateCollectionButton isLoading={false} />
+      </Box>
+    </FormProvider>
+  );
 };
 
 export default CreateCollectionForm;
