74a6aaf2b022e99b9c57f3553134d58aa8775a47
Mikhail <mk0602604@gmail.com>
2023-02-18 23:34:18 +0300

    Create and register comment entity

---
+++ server/src/comment/comment.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
