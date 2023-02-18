b0db13adec9381ca249b102120501e5281a44860
Mikhail <mk0602604@gmail.com>
2023-02-18 23:14:12 +0300

    Create comment module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 import { CollectionModule } from './collection/collection.module';
+import { CommentModule } from './comment/comment.module';
 import { PaletteModule } from './palette/palette.module';
 import { UserModule } from './user/user.module';
 import { validate } from './validation/env.validation';
@@ -17,6 +18,7 @@ import typeormConfig from './config/typeorm.config';
     UserModule,
     PaletteModule,
     CollectionModule,
+    CommentModule,
   ],
 })
 export class AppModule {}
