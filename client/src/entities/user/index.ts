f5d5f8dcd0322315a62db8d4c53a8d408e61a9ee
Mikhail <mk0602604@gmail.com>
2023-02-19 00:14:16 +0300

    Create user-avatar component

--- client/src/entities/user/index.ts
+++ client/src/entities/user/index.ts
@@ -1,2 +1,3 @@
+export { default as UserAvatar } from "./ui/user-avatar";
 export type { IUser } from "./types";
 export * from "./config/api";
---
+++ client/src/entities/user/ui/user-avatar/UserAvatar.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const UserAvatar: FC = () => {
+  return <div>UserAvatar</div>;
+};
+
+export default UserAvatar;
