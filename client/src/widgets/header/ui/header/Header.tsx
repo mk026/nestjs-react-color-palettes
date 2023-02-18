8efe9879655f4082c27dda0f9d899a4ad4c04a6b
Mikhail <mk0602604@gmail.com>
2023-02-19 00:18:31 +0300

    Refactor header component

--- client/src/widgets/header/ui/header/Header.tsx
+++ client/src/widgets/header/ui/header/Header.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 
+import AuthLinks from "../auth-links";
 import Navbar from "../navbar";
 
 const Header: FC = () => {
   return (
     <div>
       <Navbar />
+      <AuthLinks />
     </div>
   );
 };
