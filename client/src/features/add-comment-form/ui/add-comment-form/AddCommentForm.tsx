bca922ae375ff59a1ac35af47c522533ec15a7d6
Mikhail <mk0602604@gmail.com>
2023-02-23 23:37:45 +0300

    Refactor add-comment-form markup

--- client/src/features/add-comment-form/ui/add-comment-form/AddCommentForm.tsx
+++ client/src/features/add-comment-form/ui/add-comment-form/AddCommentForm.tsx
@@ -1,7 +1,24 @@
 import { FC } from "react";
+import { FormProvider } from "react-hook-form";
+import { Box } from "@mui/material";
+
+import { useAddCommentForm } from "../../lib/hooks/useAddCommentForm";
+import FormField from "../../../../shared/ui/form-field";
+import AddCommentButton from "../add-comment-button";
 
 const AddCommentForm: FC = () => {
-  return <div>AddCommentForm</div>;
+  const methods = useAddCommentForm();
+
+  const addCommentHandler = (data: any) => console.log(data);
+
+  return (
+    <FormProvider {...methods}>
+      <Box component="form" onSubmit={methods.handleSubmit(addCommentHandler)}>
+        <FormField name="content" multiline />
+        <AddCommentButton isLoading={false} />
+      </Box>
+    </FormProvider>
+  );
 };
 
 export default AddCommentForm;
