8077dd0a4e9f5e6b0a558f8f732d9f531bed8a8f
Mikhail <mk0602604@gmail.com>
2023-02-23 23:44:39 +0300

    Refactor update-comment-form markup

--- client/src/features/update-comment-form/ui/update-comment-form/UpdateCommentForm.tsx
+++ client/src/features/update-comment-form/ui/update-comment-form/UpdateCommentForm.tsx
@@ -1,7 +1,29 @@
+import { Box } from "@mui/material";
 import { FC } from "react";
+import { FormProvider } from "react-hook-form";
+
+import { useUpdateCommentForm } from "../../lib/hooks/useUpdateCommentForm";
+import FormField from "../../../../shared/ui/form-field";
+import UpdateCommentButton from "../update-comment-button";
 
 const UpdateCommentForm: FC = () => {
-  return <div>UpdateCommentForm</div>;
+  const methods = useUpdateCommentForm();
+
+  const updateCommentHandler = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <Box
+        component="form"
+        onSubmit={methods.handleSubmit(updateCommentHandler)}
+      >
+        <FormField name="content" multiline />
+        <UpdateCommentButton isLoading={false} />
+      </Box>
+    </FormProvider>
+  );
 };
 
 export default UpdateCommentForm;
