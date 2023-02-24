51ea97ff36489c553ac7682310530fa020fdcd88
Mikhail <mk0602604@gmail.com>
2023-02-24 22:45:42 +0300

    Refactor signup validation schema
    Use config constants for fields length validation

--- client/src/features/signup-form/lib/helpers/signupValidation.ts
+++ client/src/features/signup-form/lib/helpers/signupValidation.ts
@@ -1,10 +1,31 @@
 import * as yup from "yup";
 
+import {
+  USER_BIO_MAX_LENGTH,
+  USER_BIO_MIN_LENGTH,
+  USER_NAME_MAX_LENGTH,
+  USER_NAME_MIN_LENGTH,
+  USER_PASSWORD_MAX_LENGTH,
+  USER_PASSWORD_MIN_LENGTH,
+} from "../../../../entities/user";
+
 export const signupValidationSchema = yup.object({
-  name: yup.string().min(1).max(100).required(),
-  bio: yup.string().min(1).max(1000).optional(),
+  name: yup
+    .string()
+    .min(USER_NAME_MIN_LENGTH)
+    .max(USER_NAME_MAX_LENGTH)
+    .required(),
+  bio: yup
+    .string()
+    .min(USER_BIO_MIN_LENGTH)
+    .max(USER_BIO_MAX_LENGTH)
+    .optional(),
   email: yup.string().email().required(),
-  password: yup.string().min(8).max(100).required(),
+  password: yup
+    .string()
+    .min(USER_PASSWORD_MIN_LENGTH)
+    .max(USER_PASSWORD_MAX_LENGTH)
+    .required(),
   confirmPassword: yup
     .string()
     .required()
