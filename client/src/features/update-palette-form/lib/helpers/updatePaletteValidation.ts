b13bb2465be49b925b139c684c4c2e49b6d328af
Mikhail <mk0602604@gmail.com>
2023-02-24 22:47:48 +0300

    Refactor update-comment/palette validation schemas
    Use config constants for fields length validation

--- client/src/features/update-palette-form/lib/helpers/updatePaletteValidation.ts
+++ client/src/features/update-palette-form/lib/helpers/updatePaletteValidation.ts
@@ -1,6 +1,16 @@
 import * as yup from "yup";
 
+import {
+  PALETTE_TITLE_MAX_LENGTH,
+  PALETTE_TITLE_MIN_LENGTH,
+} from "../../../../entities/palette";
+
 export const updatePaletteValidationSchema = yup.object({
-  title: yup.string().email().optional(),
+  title: yup
+    .string()
+    .min(PALETTE_TITLE_MIN_LENGTH)
+    .max(PALETTE_TITLE_MAX_LENGTH)
+    .email()
+    .optional(),
   colors: yup.array().optional(),
 });
