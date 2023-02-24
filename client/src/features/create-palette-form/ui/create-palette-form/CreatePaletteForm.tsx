66c201432005bdbd95335da9ffb66d48dae1efcf
Mikhail <mk0602604@gmail.com>
2023-02-24 23:39:12 +0300

    Refactor create-palette form markup

--- client/src/features/create-palette-form/ui/create-palette-form/CreatePaletteForm.tsx
+++ client/src/features/create-palette-form/ui/create-palette-form/CreatePaletteForm.tsx
@@ -1,21 +1,18 @@
 import { FC } from "react";
-import { FormProvider } from "react-hook-form";
-import { Box } from "@mui/material";
 
 import { useCreatePaletteForm } from "../../lib/hooks/useCreatePaletteForm";
 import CreatePaletteButton from "../create-palette-button";
 import FormField from "../../../../shared/ui/form-field";
+import Form from "../../../../shared/ui/form";
 
 const CreatePaletteForm: FC = () => {
   const { formMethods, onSubmit, isLoading } = useCreatePaletteForm();
 
   return (
-    <FormProvider {...formMethods}>
-      <Box component="form" onSubmit={onSubmit}>
-        <FormField label="Title" name="title" />
-        <CreatePaletteButton isLoading={isLoading} />
-      </Box>
-    </FormProvider>
+    <Form formMethods={formMethods} onSubmit={onSubmit}>
+      <FormField label="Title" name="title" />
+      <CreatePaletteButton isLoading={isLoading} />
+    </Form>
   );
 };
 
