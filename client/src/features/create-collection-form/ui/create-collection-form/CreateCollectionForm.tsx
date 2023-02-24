b66f34633d84ba4e26b2a5d4ebf69d3beed26151
Mikhail <mk0602604@gmail.com>
2023-02-24 23:43:21 +0300

    Refactor create-collection form markup

--- client/src/features/create-collection-form/ui/create-collection-form/CreateCollectionForm.tsx
+++ client/src/features/create-collection-form/ui/create-collection-form/CreateCollectionForm.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
-import { FormProvider } from "react-hook-form";
-import { Box } from "@mui/material";
 
 import { useCreateCollectionForm } from "../../lib/hooks/useCreateCollectionForm";
 import CreateCollectionButton from "../create-collection-button";
 import FormField from "../../../../shared/ui/form-field";
+import Form from "../../../../shared/ui/form";
 
 const CreateCollectionForm: FC = () => {
   const methods = useCreateCollectionForm();
@@ -14,15 +13,13 @@ const CreateCollectionForm: FC = () => {
   };
 
   return (
-    <FormProvider {...methods}>
-      <Box
-        component="form"
-        onSubmit={methods.handleSubmit(createCollectionHandler)}
-      >
-        <FormField label="Title" name="title" />
-        <CreateCollectionButton isLoading={false} />
-      </Box>
-    </FormProvider>
+    <Form
+      formMethods={methods}
+      onSubmit={methods.handleSubmit(createCollectionHandler)}
+    >
+      <FormField label="Title" name="title" />
+      <CreateCollectionButton isLoading={false} />
+    </Form>
   );
 };
 
