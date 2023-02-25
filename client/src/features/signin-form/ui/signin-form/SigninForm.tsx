405d703c3ced40c074d3ffb2eefe0d84c7816322
Mikhail <mk0602604@gmail.com>
2023-02-25 23:02:47 +0300

    Refactor signup/signin forms markup

--- client/src/features/signin-form/ui/signin-form/SigninForm.tsx
+++ client/src/features/signin-form/ui/signin-form/SigninForm.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
-import { FormProvider } from "react-hook-form";
-import { Box } from "@mui/material";
 
+import { useSigninForm } from "../../lib/hooks/useSigninForm";
+import Form from "../../../../shared/ui/form";
 import FormField from "../../../../shared/ui/form-field";
 import SigninButton from "../signin-button";
-import { useSigninForm } from "../../lib/hooks/useSigninForm";
 
 const SigninForm: FC = () => {
   const methods = useSigninForm();
@@ -14,13 +13,11 @@ const SigninForm: FC = () => {
   };
 
   return (
-    <FormProvider {...methods}>
-      <Box component="form" onSubmit={methods.handleSubmit(signinHandler)}>
-        <FormField label="Email" name="email" type="email" />
-        <FormField label="Password" name="password" type="password" />
-        <SigninButton isLoading={false} />
-      </Box>
-    </FormProvider>
+    <Form formMethods={methods} onSubmit={methods.handleSubmit(signinHandler)}>
+      <FormField label="Email" name="email" type="email" />
+      <FormField label="Password" name="password" type="password" />
+      <SigninButton isLoading={false} />
+    </Form>
   );
 };
 
--- client/src/features/signup-form/ui/signup-form/SignupForm.tsx
+++ client/src/features/signup-form/ui/signup-form/SignupForm.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
-import { FormProvider } from "react-hook-form";
-import { Box } from "@mui/material";
 
+import { useSignupForm } from "../../lib/hooks/useSignupForm";
+import Form from "../../../../shared/ui/form";
 import FormField from "../../../../shared/ui/form-field";
 import SignupButton from "../signup-button";
-import { useSignupForm } from "../../lib/hooks/useSignupForm";
 
 const SignupForm: FC = () => {
   const methods = useSignupForm();
@@ -14,20 +13,18 @@ const SignupForm: FC = () => {
   };
 
   return (
-    <FormProvider {...methods}>
-      <Box component="form" onSubmit={methods.handleSubmit(signupHandler)}>
-        <FormField label="Name" name="name" />
-        <FormField label="About yourself" name="bio" multiline />
-        <FormField label="Email" name="email" type="email" />
-        <FormField label="Password" name="password" type="password" />
-        <FormField
-          label="Confirm Password"
-          name="confirmPassword"
-          type="password"
-        />
-        <SignupButton isLoading={false} />
-      </Box>
-    </FormProvider>
+    <Form formMethods={methods} onSubmit={methods.handleSubmit(signupHandler)}>
+      <FormField label="Name" name="name" />
+      <FormField label="About yourself" name="bio" multiline />
+      <FormField label="Email" name="email" type="email" />
+      <FormField label="Password" name="password" type="password" />
+      <FormField
+        label="Confirm Password"
+        name="confirmPassword"
+        type="password"
+      />
+      <SignupButton isLoading={false} />
+    </Form>
   );
 };
 
