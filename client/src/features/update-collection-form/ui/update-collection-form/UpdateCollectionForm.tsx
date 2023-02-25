951436dc639252259aaf7c5d15423e4395ba756c
Mikhail <mk0602604@gmail.com>
2023-02-25 23:20:36 +0300

    Refactor update-collection form markup

--- client/src/features/update-collection-form/ui/update-collection-form/UpdateCollectionForm.tsx
+++ client/src/features/update-collection-form/ui/update-collection-form/UpdateCollectionForm.tsx
@@ -1,7 +1,25 @@
 import { FC } from "react";
 
+import { useUpdateCollectionForm } from "../../lib/hooks/useUpdateCollectionForm";
+import Form from "../../../../shared/ui/form";
+import FormField from "../../../../shared/ui/form-field";
+import UpdateCollectionButton from "../update-collection-button";
+
 const UpdateCollectionForm: FC = () => {
-  return <div>UpdateCollectionForm</div>;
+  const methods = useUpdateCollectionForm();
+
+  const updateCollectionHandler = (values: any) => {
+    console.log(values);
+  };
+  return (
+    <Form
+      formMethods={methods}
+      onSubmit={methods.handleSubmit(updateCollectionHandler)}
+    >
+      <FormField label="Title" name="title" />
+      <UpdateCollectionButton isLoading={false} />
+    </Form>
+  );
 };
 
 export default UpdateCollectionForm;
