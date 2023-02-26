44512e8f9a570e858654031523fb52cb3b085cab
Mikhail <mk0602604@gmail.com>
2023-02-26 22:17:19 +0300

    Implement comment service CRUD methods

--- server/src/comment/comment.service.ts
+++ server/src/comment/comment.service.ts
@@ -1,32 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
+import { Comment } from './comment.entity';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { GetCommentsDto } from './dto/get-comments.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>,
+  ) {}
+
   getComments(getCommentsDto: GetCommentsDto) {
-    return `Return all comments for post with id ${getCommentsDto.postId}`;
+    return this.commentRepository.find({
+      where: {
+        palette: { id: getCommentsDto.paletteId },
+      },
+    });
   }
 
   getComment(id: number) {
-    return `Return comment with id ${id}`;
+    return this.commentRepository.findOne({ where: { id } });
   }
 
-  createComment(createCommentDto: CreateCommentDto, userId: number) {
-    return `Create new comment ${createCommentDto.content}`;
+  async createComment(createCommentDto: CreateCommentDto, userId: number) {
+    const comment = this.commentRepository.create({
+      palette: { id: createCommentDto.paletteId },
+      content: createCommentDto.content,
+      author: { id: userId },
+    });
+    await this.commentRepository.save(comment);
   }
 
-  updateComment(
+  async updateComment(
     id: number,
     updateCommentDto: UpdateCommentDto,
     userId: number,
   ) {
-    return `Update comment with id ${id} ${updateCommentDto.content}`;
+    const result = await this.commentRepository.update(
+      { id, author: { id: userId } },
+      updateCommentDto,
+    );
+    if (result.affected === 0) {
+      throw new NotFoundException(`Comment with id ${id} not found`);
+    }
   }
 
-  deleteComment(id: number, userId: number) {
-    return `Delete comment with id ${id}`;
+  async deleteComment(id: number, userId: number) {
+    const result = await this.commentRepository.delete({
+      id,
+      author: { id: userId },
+    });
+    if (result.affected === 0) {
+      throw new NotFoundException(`Comment with id ${id} not found`);
+    }
   }
 }
