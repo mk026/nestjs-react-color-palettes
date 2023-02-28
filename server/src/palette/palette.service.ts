90da1f7b0316a5c24aaec7b124e6eb7c420ccb86
Mikhail <mk0602604@gmail.com>
2023-02-28 23:35:21 +0300

    Implement palette service CRUD methods

--- server/src/palette/palette.service.ts
+++ server/src/palette/palette.service.ts
@@ -1,31 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
+import { Palette } from './palette.entity';
 import { CreatePaletteDto } from './dto/create-palette.dto';
 import { UpdatePaletteDto } from './dto/update-palette.dto';
 
 @Injectable()
 export class PaletteService {
+  constructor(
+    @InjectRepository(Palette)
+    private readonly paletteRepository: Repository<Palette>,
+  ) {}
+
   getPalettes() {
-    return 'Return all palettes';
+    return this.paletteRepository.find();
   }
 
   getPalette(id: number) {
-    return `Return palette with id ${id}`;
+    return this.paletteRepository.findOne({ where: { id } });
   }
 
-  createPalette(createPaletteDto: CreatePaletteDto, userId: number) {
-    return 'Create new palette';
+  async createPalette(createPaletteDto: CreatePaletteDto, userId: number) {
+    const palette = this.paletteRepository.create({
+      title: createPaletteDto.title,
+      colors: createPaletteDto.colors,
+      author: { id: userId },
+    });
+    await this.paletteRepository.save(palette);
   }
 
-  updatePalette(
+  async updatePalette(
     id: number,
     updatePaletteDto: UpdatePaletteDto,
     userId: number,
   ) {
-    return `Update palette with id ${id}`;
+    const result = await this.paletteRepository.update(
+      { id, author: { id: userId } },
+      updatePaletteDto,
+    );
+    if (result.affected === 0) {
+      throw new NotFoundException(`Palette with id ${id} not found`);
+    }
   }
 
-  deletePalette(id: number, userId: number) {
-    return `Delete palette with id ${id}`;
+  async deletePalette(id: number, userId: number) {
+    const result = await this.paletteRepository.delete({
+      id,
+      author: { id: userId },
+    });
+    if (result.affected === 0) {
+      throw new NotFoundException(`Palette with id ${id} not found`);
+    }
   }
 }
