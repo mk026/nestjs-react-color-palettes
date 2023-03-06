99327c09e0149800882502509b2791fd98988409
Mikhail <mk0602604@gmail.com>
2023-03-06 23:12:00 +0300

    Create auth-response interface

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -3,13 +3,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninCredentialsDto } from './dto/signin-credentials.dto';
 import { SignupCredentialsDto } from './dto/signup-credentials.dto';
+import { AuthResponse } from './interfaces/auth-response.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() signupCredentialsDto: SignupCredentialsDto) {
+  signup(
+    @Body() signupCredentialsDto: SignupCredentialsDto,
+  ): Promise<AuthResponse> {
     return this.authService.signup(signupCredentialsDto);
   }
 
