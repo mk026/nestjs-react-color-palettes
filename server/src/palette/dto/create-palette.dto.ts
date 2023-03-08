0e79e9398e8121af1ab7e19f7f863c54dbda77bb
Mikhail <mk0602604@gmail.com>
2023-03-08 22:14:24 +0300

    Add description field in create-palette dto

--- server/src/palette/dto/create-palette.dto.ts
+++ server/src/palette/dto/create-palette.dto.ts
@@ -1,10 +1,15 @@
-import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsOptional, MaxLength, MinLength } from 'class-validator';
 
 export class CreatePaletteDto {
   @IsNotEmpty()
   @MinLength(1)
   @MaxLength(100)
   readonly title: string;
+
+  @IsOptional()
+  @MinLength(1)
+  @MaxLength(1000)
+  readonly description?: string;
 
   @IsNotEmpty({ each: true })
   readonly colors: string[];
