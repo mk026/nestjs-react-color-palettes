8cd6ecedd993119f858c1641b3552735fc795df5
Mikhail <mk0602604@gmail.com>
2023-03-13 13:12:35 +0300

    Remove redundant favorite module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
-import { FavoriteModule } from './favorite/favorite.module';
 import { CollectionModule } from './collection/collection.module';
 import { CommentModule } from './comment/comment.module';
 import { PaletteModule } from './palette/palette.module';
@@ -20,7 +19,6 @@ import typeormConfig from './config/typeorm.config';
     PaletteModule,
     CollectionModule,
     CommentModule,
-    FavoriteModule,
   ],
 })
 export class AppModule {}
